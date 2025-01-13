1cbf96816d2ae3abe78eacc5a644b961fd2a2776
bobbymilo12 <134400920+bobbymilo12@users.noreply.github.com>
2025-01-13 03:38:05 +0000

    e

--- SPACoursework/script.js
+++ SPACoursework/script.js
@@ -1,14 +1,24 @@
+function showContent(contentId) {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById(contentId).style.display = 'block';
+}
+
+function hideContent() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('content').style.display = 'none';
+}
+
 document.getElementById('flashcardForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
     const question = document.getElementById('question').value;
     const answer = document.getElementById('answer').value;
-    
+
     const flashcard = {
         question: question,
         answer: answer
     };
-    
+
     fetch('/saveFlashcard', {
         method: 'POST',
         headers: {
